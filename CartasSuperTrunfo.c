188fc2e97695745c734729fc62303d92b6422fb7
freitas2k24k <felipeborges715@gmail.com>
2025-09-10 15:03:09 +0000

    crei metade do jogo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,9 +1,30 @@
 #include <stdio.h>
 
 int main() {
-    
-printf ("Desafio do xadrez! \n");
+char estado [10];
+char codigo [5];
+char cidade [20];
+int populacao;
+int Pontosturistico;
+float area;
+float pib;
 
+
+
+printf("Digite Seu Estado: \n");
+scanf("%s", &estado);
+
+printf("Código: \n");
+scanf("%s", &codigo);
+
+printf("Nome Da Cidade: \n");
+scanf("%s", &cidade);
+
+printf("Total de Habitantes: \n");
+scanf("%d", &populacao);
+
+printf("Area: \n");
+scanf("%f", &area);
 
 
 
